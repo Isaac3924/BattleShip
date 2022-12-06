e3ea79705766420de655bd837d5010e5e55c0829
Dawson Timmons <DawsonTimmons@gmail.com>
2022-12-06 16:16:00 -0700

    Add atributes to ship class and test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,8 +3,16 @@ require './lib/ship'
 
 RSpec.describe Ship do
     it '1. exists' do
-        cruiser = Ship.new
+        cruiser = Ship.new('cruiser', 3)
 
         expect(cruiser).to be_an_instance_of(Ship)
+    end
+
+    it '2. has attributes' do
+        cruiser = Ship.new('cruiser', 3)
+
+        expect(cruiser.name).to eq('cruiser')
+        expect(cruiser.length).to eq (3)
+        expect(cruiser.health).to eq (3)
     end
 end
